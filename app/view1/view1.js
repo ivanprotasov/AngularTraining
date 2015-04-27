2434b889b43fc1747a2848f0c72370c1f65d2eb8
ivanprotasov <ivanprotasov@narod.ru>
2015-04-27 23:08:22 +0300

    111

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,11 +5,29 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl as invoice'
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
-      $scope.qty = 5;
-      $scope.cost = 2;
+.controller('View1Ctrl', [ function() {
+
+        this.qty = 1;
+        this.cost = 2;
+        this.inCurr = 'EUR';
+        this.currencies = ['USD', 'EUR', 'CNY'];
+        this.usdToForeignRates = {
+            USD: 1,
+            EUR: 0.74,
+            CNY: 6.09
+        };
+
+        this.total = function total(outCurr) {
+            return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+        };
+        this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+            return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+        };
+        this.pay = function pay() {
+            window.alert("Thanks!");
+        };
 }]);
