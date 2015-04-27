90f88e54ef1d5a5c658c9d4a12d6f1203bd7d2ff
Ivan_Pratasau <ivanpratasau@epam.com>
2015-04-27 18:09:56 +0300

    Test

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,7 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+      $scope.qty = 5;
+      $scope.cost = 2;
 }]);
